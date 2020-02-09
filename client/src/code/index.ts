cf60fd0d4d55ef694143a9980a256c2ebf95b9cb
Lorenzo Fontoura <lorenzo@nioel.com>
2020-02-10 07:37:45 +1000

    Add pixi.js

--- client/src/code/index.ts
+++ client/src/code/index.ts
@@ -1,9 +1,36 @@
 import "../style/index.scss";
+import * as PIXI from 'pixi.js'
 
-const root: HTMLElement = document.getElementById("root") as HTMLElement;
-const title = document.createElement("h1");
+class Client {
 
-title.innerHTML = "COMMUNITY GAME!!!";
-root.append(title);
+    private root: HTMLDivElement = document.getElementById("root") as HTMLDivElement;
+    private canvas: HTMLCanvasElement = document.getElementById("view") as HTMLCanvasElement;
+    private app: PIXI.Application;
 
-console.log("YAAY");
+    public constructor() {
+        this.app = new PIXI.Application({
+            view: this.canvas,
+            width: window.innerWidth,
+            height: window.innerHeight,
+            
+            backgroundColor: 0x131313
+        });
+
+        this.windowEvents();
+
+        const text = new PIXI.Text("community game prototype!");
+        text.style.fill = 0xf0f0f0;
+        text.x = (window.innerWidth/2) - (text.width/2);
+        text.y = (window.innerHeight/2) - (text.height/2);
+
+        this.app.stage.addChild(text);
+    }
+
+    private windowEvents() {
+        window.addEventListener("resize", () => {
+            this.app.renderer.resize(window.innerWidth, window.innerHeight);
+        });
+    }
+}
+
+const client = new Client();
