b51d799ca5c64f0719f917d18c050f737c0d4f2a
Lorenzo Fontoura <lorenzo@nioel.com>
2020-04-22 11:30:53 +1000

    Engine cargo setup

---
+++ engine/src/types/transform.rs
@@ -0,0 +1,25 @@
+use nalgebra::*;
+
+pub struct Transform {
+    position: Vector2<f32>,
+    forward: Vector2<f32>,
+}
+
+impl Default for Transform {
+    fn default() -> Transform {
+        Transform {
+            position: Vector2::new(0.0, 0.0),
+            forward: Vector2::new(1.0, 0.0)
+        }
+    }
+}
+
+impl Transform {
+    pub fn get_position(self) -> Vector2<f32> {
+        self.position
+    }
+
+    pub fn forward(self) -> Vector2<f32> {
+        self.forward
+    }
+}
