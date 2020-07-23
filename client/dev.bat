0cde79a11cbce0391fdb36d623dc76edcd655c44
Lorenzo Fontoura <lorenzo@nioel.com>
2020-07-23 18:43:03 +1000

    Update dev script to setup wasm32-unknown-unknown target

--- client/dev.bat
+++ client/dev.bat
@@ -1,5 +1,6 @@
 setlocal
+rustup target add wasm32-unknown-unknown
 cargo install cargo-watch
 start "bunker.nz build slave" cargo watch -x "build --target wasm32-unknown-unknown"
 cd /d %~dp0\dws
-start "bnker.nz dev server" node index.js
+start "bunker.nz dev server" node index.js
